f4e5b210625ded71c179e3154ae128ee9b075526
Gusten <johan.gusten@gmail.com>
2016-02-07 17:35:58 +0100

    First commit (again)

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <GL\glut.h>
+
+void display();
+
+int main(int argc, char *argv[]) {
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA);
+	glutInitWindowSize(400, 400);
+	glutInitWindowPosition(100, 100);
+	glutCreateWindow("GGRT");
+	glutDisplayFunc(display);
+	glutMainLoop();
+	return 0;
+}
+
+void display(void) {
+	glClearColor(1.0, 0.0, 0.0, 1.0);
+	glClear(GL_COLOR_BUFFER_BIT);
+	glFlush();
+}
