09e8c9d9e4b13b0c95cc0381eda1391b34c4b22b
Gusten <johan.gusten@gmail.com>
2016-02-08 20:29:31 +0100

    Adds basic shader functionality and refactors display method to use the shader and the vertex data provided. Also adds an FPS counter

--- main.cpp
+++ main.cpp
@@ -1,19 +1,26 @@
-#ifdef WIN32
-#include <windows.h>
-#endif
-
 #include <GL\glew.h>
 #include <GL\glut.h>
 #include <string>
 #include <iostream>
 #include <fstream>
 
-const char *readShaderFromFile(const char *filePath) {
+// The defaault shader program and the vertex array object are global variables
+// so that the display method can properly use them.
+GLuint defShaderProgram;
+GLuint vertexArrayObject;
+
+// Variables to hold FPS data
+int framesSinceLastCheck;
+float timeAtLastCheck;
+int fps;
+
+
+std::string readShaderFromFile(const char *filePath) {
 	std::string content;
 	std::ifstream fStream(filePath);
 
 	if (!fStream.is_open()) {
-		std::cerr << "Could not read file: " << filePath << ". Are you sure it exists??" << std::endl;
+		std::cerr << "Could not open: " << filePath << ". Typos much??\n";
 		return "";
 	}
 
@@ -24,7 +31,32 @@ const char *readShaderFromFile(const char *filePath) {
 	}
 
 	fStream.close();
-	return content.c_str();
+	return content;
+}
+
+void compileShader(GLuint shaderObject) {
+	glCompileShader(shaderObject);
+
+	// Get log file and print it, even if there are no errors since there could be warnings
+	// helping us debug the shaders
+	GLint logBuffLen;
+	glGetShaderiv(shaderObject, GL_INFO_LOG_LENGTH, &logBuffLen);
+	if (logBuffLen > 1)
+	{
+		GLchar* log_string = new char[logBuffLen + 1];
+		glGetShaderInfoLog(shaderObject, logBuffLen, 0, log_string);
+		std::cout << log_string;
+
+		delete log_string;
+	}
+
+	// Check if the shader successfully compiled
+	GLint shaderIsCompiled;
+	glGetShaderiv(shaderObject, GL_COMPILE_STATUS, &shaderIsCompiled);
+	if (shaderIsCompiled != GL_TRUE)
+	{
+		std::cerr << "Shader could not compile\n";
+	}
 }
 
 
@@ -48,15 +80,46 @@ void initGL() {
 	glBindBuffer(GL_ARRAY_BUFFER, positionBuffer);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(positions), positions, GL_STATIC_DRAW);
 
+	//Create a vertex array object
+	glGenVertexArrays(1, &vertexArrayObject);
+	glBindVertexArray(vertexArrayObject);
+	glBindBuffer(GL_ARRAY_BUFFER, positionBuffer);
+	glVertexAttribPointer(0, 3, GL_FLOAT, false, 0, 0);
+	glEnableVertexAttribArray(0);
+
 	//TODO: Init vertex/fragment shaders
 	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
 	GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
 
-	const char *vertShaderString = readShaderFromFile("simple.vert");
-	const char *fragShaderString = readShaderFromFile("simple.frag");
+	std::string vertShaderString = readShaderFromFile("simple.vert");
+	std::string fragShaderString = readShaderFromFile("simple.frag");
+	const char *vertShaderChars = vertShaderString.c_str();
+	const char *fragShaderChars = fragShaderString.c_str();
 
-	glShaderSource(vertexShader, 1, &vertShaderString, NULL);
-	glShaderSource(fragmentShader, 1, &fragShaderString, NULL);
+	glShaderSource(vertexShader, 1, &vertShaderChars, NULL);
+	glShaderSource(fragmentShader, 1, &fragShaderChars, NULL);
+
+	compileShader(vertexShader);
+	compileShader(fragmentShader);
+
+	defShaderProgram = glCreateProgram();
+	glAttachShader(defShaderProgram, vertexShader);
+	glAttachShader(defShaderProgram, fragmentShader);
+
+	// Might be counterintuitive to delete the shaders here but they live on in our shaderprogram
+	// so basically we just free up some unused memory.
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+
+	glBindAttribLocation(defShaderProgram, 0, "position");
+
+	glLinkProgram(defShaderProgram);
+
+	GLint programIsLinked;
+	glGetProgramiv(defShaderProgram, GL_LINK_STATUS, &programIsLinked);
+	if (!programIsLinked) {
+		std::cout << "Error when linking default shader program\n";
+	}
 
 	//TODO: Init texture to hold tracing results(image)
 	//TODO: 
@@ -64,7 +127,7 @@ void initGL() {
 
 
 void display(void) {
-	glClearColor(1.0, 0.0, 0.0, 1.0);
+	glClearColor(0.0, 0.0, 0.0, 1.0);
 	glClear(GL_COLOR_BUFFER_BIT);
 
 	int w = glutGet((GLenum)GLUT_WINDOW_WIDTH);
@@ -72,7 +135,39 @@ void display(void) {
 	glViewport(0, 0, w, h);
 	glDisable(GL_CULL_FACE);
 	
+	// Set our shaderprogram and then draw the triangles that contains the raytraced texture/image.
+	//TODO: Actually draw a texture.............
+	glUseProgram(defShaderProgram);
+	glBindVertexArray(vertexArrayObject);
+	glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
+	glUseProgram(0);
+
+	// Print fps in bottom left corner
+	std::string fpsString = std::to_string(fps);
+	for (int i = 0; i < fpsString.length(); i++) {
+		glWindowPos2i(i*12, 0);
+		glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, fpsString[i]);
+	}
+
 	glutSwapBuffers();
+}
+
+void idle()
+{
+	//Calculate fps to display in the scene
+	framesSinceLastCheck++;
+	float currentTime = glutGet(GLUT_ELAPSED_TIME);
+	float timeSinceLastCheck = currentTime - timeAtLastCheck;
+
+	if (timeSinceLastCheck > 1000.0f) {
+		//Calculate with floats and then cast to int
+		fps = ((float)framesSinceLastCheck) / (timeSinceLastCheck / 1000.0f);
+
+		timeAtLastCheck = currentTime;
+		framesSinceLastCheck = 0;
+	}
+
+	glutPostRedisplay();
 }
 
 
@@ -83,6 +178,7 @@ int main(int argc, char *argv[]) {
 	glutInitWindowPosition(100, 100);
 	glutCreateWindow("GGRT");
 	glutDisplayFunc(display);
+	glutIdleFunc(idle);
 
 	initGL();
 
--- simple.frag
+++ simple.frag
@@ -1,15 +1,15 @@
 #version 130
 
-// required by GLSL spec Sect 4.5.3 (though nvidia does not, amd does)
 precision highp float;
 
-uniform sampler2D framebuffer; 
-uniform int framebufferSamples; 
+//uniform sampler2D framebuffer; 
+//uniform int framebufferSamples; 
 
-in vec2 texCoord; 
+//in vec2 texCoord; 
 out vec4 fragmentColor;
 
 void main() 
 {
-	fragmentColor = (1.0 / float(framebufferSamples)) * texture2D(framebuffer, texCoord);
+	//fragmentColor = (1.0 / float(framebufferSamples)) * texture2D(framebuffer, texCoord);
+	fragmentColor = vec4(0.0f, 1.0f, 0.0f, 0.0f);
 }
